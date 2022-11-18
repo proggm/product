2396ce17ea5f61e7511b43a06505449ae7dfa5fc
viktor <moiseev0506@mail.ru>
2022-11-18 20:02:13 +0300

    version 1

---
+++ products/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<products> products = new List<products>
+            { 
+                new products("Chicken","450",10, 14),
+                new products("Cucumber","90",5,2),
+                new products("Tomatoes","110",10, 9),
+                new products("Coca-cola","150",12,13),
+                new products($"Apple","75",38,15),
+            };
+            var first = products.Where(item => item.dodo>=item.expirationdate).ToList();
+            foreach(var item in first)
+            {
+                Console.WriteLine($"Cрок годности истек у: {item.name}");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ products/products.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace products
+{
+    internal class products
+    {
+        public products(string name, string price, int expirationdate,int dodo)
+        {
+            this.name = name;
+            this.price = price;
+            this.expirationdate = expirationdate;
+            this.dodo = dodo;
+        }
+
+        public string name { get; set; }
+        public string price { get; set; }  
+        public int expirationdate { get; set; }
+        public int dodo { get; set; }
+    }
+}
